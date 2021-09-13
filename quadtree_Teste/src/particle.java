77515229c67ea13ee29f9ae9a0aaae0051ca0830
Thiago Cruz <tcruz.gsantos@gmail.com>
2021-09-13 19:20:12 -0300

    Movimentação de partículas
    Implementação da movimentação de partículas (Problema de partículas piscando, não sei o motivo)

--- quadtree_Teste/src/particle.java
+++ quadtree_Teste/src/particle.java
@@ -6,20 +6,41 @@ public class particle  {
 
 	Random random = new Random();
 	
-	int x;
-	int y;
-	int h = 10;
-	int w = 10;
+		int x;
+		int y;
+		int h = 6;
+		int w = 6;
+		int speedX;
+		int speedY;
 	
 	public particle (int _x, int _y) {
 		
 		this.x = random.nextInt(_x);
 		this.y = random.nextInt(_y);
 		
+		while (speedX == 0 && speedY == 0) {
+		this.speedX = random.nextInt(3)-1;
+		this.speedY = random.nextInt(3)-1;		
+		}
+	}
+	
+	public void Update () {
+		
+		this.x += this.speedX;
+		this.y += this.speedY;
+		
+		if (this.x >= 1000 || this.x <= 31) {
+			this.speedX *= -1;
+		}
+		if (this.y >= 1000 || this.y <= 31) {
+			this.speedY *= -1;
+		}	
+		
 	}
 	
 	public void draw(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
 		g2d.fillOval(this.x - w/2, this.y - h/2, h, w);
 	}
 	
--- quadtree_Teste/src/screen.java
+++ quadtree_Teste/src/screen.java
@@ -1,29 +1,47 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class screen extends JFrame {
-
+public class screen extends JFrame { 
+	
    ArrayList<particle> particles = new ArrayList();
    int particleCount = 100;
    int screenSize = 1000;
-	
+   
 	public screen () {
 		for(int i = 0; i < particleCount; i++) {
 			particles.add(new particle(screenSize, screenSize));
 		}
 		setSize (screenSize,screenSize);
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);	
 		
+		ActionListener loop = new ActionListener() 
+		{
+		    public void actionPerformed(ActionEvent ev)
+		    {   
+		    	for(particle part : particles) {
+		    		part.Update();
+		    	}
+		       repaint();		    	
+		    }
+		};  
+		new Timer(1, loop).start();
 	}
+	
 	
 	@Override
 	public void paint (Graphics g) {
+		g.setColor(Color.gray);
+		g.fillRect(0, 0, screenSize, screenSize);
 		for(int i = 0; i < particleCount; i++) {
 			particles.get(i).draw(g);
 		}
 	}
+	
 	
 	public static void main(String args[]) {
 		
